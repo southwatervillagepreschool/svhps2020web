a428c8257fbe7d48e6063c270517f06202e200f7
philstenning <philstenning@hotmail.com>
2020-10-19 10:10:08 +0100

    filter articles by order

--- src/components/PageArticles.js
+++ src/components/PageArticles.js
@@ -1,17 +1,21 @@
 import React from "react"
 
 const PageArticles = ({ listOfArticles }) => {
+  // do not display any articles with a value less than one.
+  const filteredList = listOfArticles.filter(
+    a => a.childMarkdownRemark.frontmatter.order >= 1
+  )
+
+  console.log(filteredList)
   return (
     <>
-      {listOfArticles.map(singleArticle => {
+      {filteredList.map(singleArticle => {
         const { frontmatter, html } = singleArticle.childMarkdownRemark
         const { title } = frontmatter
         const formattedTitle = title
           ? title.replace(/ /, "_").toLowerCase()
           : ""
-        if (frontmatter.order === 0) {
-          return
-        }
+        
         return (
           <article key={title}>
             {title && <h2 id={formattedTitle}>{title}</h2>}
--- src/editable-content/home/closure.md
+++ src/editable-content/home/closure.md
@@ -1,6 +1,6 @@
 ---
 title: Preschool Closure
-order: 0  # change to 1 to show this section.
+order: 0 # change to 1 to show this section.
 ---
 
 Due to events ... we are currently closes.
