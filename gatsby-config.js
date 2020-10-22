42e1e923752f13d466574136629a27ac6487be28
philstenning <philstenning@hotmail.com>
2020-10-22 15:05:19 +0100

    add icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,24 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Southwater village preschools website",
+        short_name: "SVPS",
+        start_url: "/",
+        background_color: "#eff0f3",
+        theme_color: "#eff0f3",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.svg", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
