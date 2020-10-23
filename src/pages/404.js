4960994c158eb56b3acafaf41166fdad03f2ef62
philstenning <philstenning@hotmail.com>
2020-10-23 12:55:44 +0100

    add custom error page

---
+++ src/pages/404.js
@@ -0,0 +1,31 @@
+import React from "react"
+import styled from "styled-components"
+import { navigate } from "@reach/router"
+// import Layout from "../components/layout"
+import Layout from "../components/Layout"
+
+const Article = styled.article`
+  text-align: center;
+  p {
+    text-align: center;
+  }
+`
+const CanotFind = () => {
+  const handleNavigate = e => {
+    e.preventDefault()
+    navigate(-1)
+  }
+  return (
+    <Layout>
+      <Article>
+        <h2>Page not found</h2>
+        <p>Oops! the page you are looking for has been removed of relocated.</p>
+        <a href="/" onClick={e => handleNavigate(e)}>
+          Go Back
+        </a>
+      </Article>
+    </Layout>
+  )
+}
+
+export default CanotFind
